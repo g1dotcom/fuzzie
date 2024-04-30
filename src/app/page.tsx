cd6923acac6dedf53265af95ab028014fb2aa02a
Gökhan Süle <thegkhnsl@gmail.com>
2024-04-30 17:34:52 +0300

    public içine assetler eklendi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,28 @@ export default function Home() {
           <ContainerScroll
             titleComponent={
               <div className="flex items-center justify-center flex-col">
-                <Button></Button>
+                <Button
+                  size={"lg"}
+                  className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
+                >
+                  <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
+                    Start For Free Today
+                  </span>
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Fuzzie
+                </h1>
               </div>
             }
-          ></ContainerScroll>
+          />
         </div>
       </section>
+      <InfiniteMovingCards
+        className="md:mt-[18rem] mt-[-100px]"
+        items={clients}
+        direction="right"
+        speed="slow"
+      />
     </main>
   );
 }
---
+++ src/lib/constant.ts
@@ -0,0 +1,4 @@
+export const clients = [...new Array(10)].map((client, index) => ({
+    href: `/${index + 1}.png`,
+  }))
+  
