dbac374171f438f8aa0cde243efab79740200bd3
Gökhan Süle <thegkhnsl@gmail.com>
2024-04-30 21:53:48 +0300

    (pages) altında layout.tsx olşturuldu klasörleme yapıldı

---
+++ src/app/main/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = { children: React.ReactNode };
+
+const Layout = (props: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+      <div className="w-full">{props.children}</div>
+    </div>
+  );
+};
+
+export default Layout;
