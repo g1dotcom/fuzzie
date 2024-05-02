eefa93d79889f57bb0cc75b2403eecaa76d3e727
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-02 16:51:00 +0300

    icons içinden gelen veriler import edildi

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,3 +1,10 @@
+import Category from '@/components/icons/category'
+import Logs from '@/components/icons/clipboard'
+import Templates from '@/components/icons/cloud_download'
+import Home from '@/components/icons/home'
+import Payment from '@/components/icons/payment'
+import Settings from '@/components/icons/settings'
+import Workflows from '@/components/icons/workflows'
 
 export const clients = [...new Array(10)].map((client, index) => ({
   href: `/${index + 1}.png`,
