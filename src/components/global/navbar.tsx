4102b588efeb99b02c3514b58ca62963b890cb51
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-02 18:09:35 +0300

    oco

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = async (props: Props) => {
       </nav>
       <aside className="flex items-center gap-4">
         <Link
-          href="/main/dashboard"
+          href="/dashboard"
           className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
