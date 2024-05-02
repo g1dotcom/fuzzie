a4d2d897704c6ff983cfe342af38730ac1679587
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-02 16:48:57 +0300

    icons oluşturuldu

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Sidebar from "@/components/sidebar";
 
 type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
     <div className="flex overflow-hidden h-screen">
+      <Sidebar />
       <div className="w-full">{props.children}</div>
     </div>
   );
