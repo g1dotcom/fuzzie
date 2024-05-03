ce3f3b9e9e9231e094649b32b49308e585064eb6
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-03 16:33:15 +0300

    profil için gerekli fonksiyonlar ve ui

---
+++ src/components/forms/profile-form.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React, { use, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { EditUserProfileSchema } from "@/lib/types";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+
+type Props = {};
+
+const ProfileForm = (props: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof EditUserProfileSchema>>({
+    mode: "onChange",
+    resolver: zodResolver(EditUserProfileSchema),
+    defaultValues: {
+      name: user.name,
+      email: user.email,
+    },
+  });
+
+  return <div>ProfileForm</div>;
+};
+
+export default ProfileForm;
