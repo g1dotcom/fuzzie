a691d34eaf62c28a3293302b77be38f252746810
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-06 13:32:42 +0300

    modeller düzenlendi ve profile picture dosyası oluşturuldu

---
+++ src/app/(main)/(pages)/settings/_components/profile-picture.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const ProfilePicture = (props: Props) => {
+  return <div>ProfilePicture</div>;
+};
+
+export default ProfilePicture;
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@prisma/client'
 
+// Global bir değişken bildirimi yapılıyor.
+// Bu değişken, tüm dosyaların erişebileceği şekilde 'global' olarak tanımlanıyor.
 declare global {
   var prisma: PrismaClient | undefined
 }
 
+// 'db' adında bir sabit oluşturuluyor.
+// Bu sabit, 'globalThis.prisma' değişkeninin değerini alıyor.
+// Eğer 'globalThis.prisma' değişkeni tanımlı değilse, yeni bir PrismaClient örneği oluşturuluyor.
 export const db = globalThis.prisma || new PrismaClient()
 
+// Eğer 'NODE_ENV' değişkeni 'production' değilse,
+// 'globalThis.prisma' değişkeni 'db' sabitine eşitleniyor.
+// Bu, geliştirme ortamında her yerde 'prisma' değişkenine erişmek için kullanılıyor.
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
