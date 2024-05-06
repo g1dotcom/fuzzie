b47b51cc1ca74035e7df706697db4ed83a08cb77
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-06 14:12:29 +0300

    removeprofileımage fonk.

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -5,6 +5,19 @@ import ProfilePicture from "./_components/profile-picture";
 type Props = {};
 
 const Settings = (props: Props) => {
+  const removeProfileImage = async () => {
+    "use server";
+    const response = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        profileImage: "",
+      },
+    });
+    return response;
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
