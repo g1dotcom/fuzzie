4f621ec97e7cf32f7d213f71be418ce429de63bf
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-09 13:54:12 +0300

    libs içine elementler eklendi

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -146,35 +146,35 @@ export const EditorCanvasDefaultCardTypes = {
   },
 }
 
-// export const CONNECTIONS: Connection[] = [
-//   {
-//     title: 'Google Drive',
-//     description: 'Connect your google drive to listen to folder changes',
-//     image: '/googleDrive.png',
-//     connectionKey: 'googleNode',
-//     alwaysTrue: true,
-//   },
-//   {
-//     title: 'Discord',
-//     description: 'Connect your discord to send notification and messages',
-//     image: '/discord.png',
-//     connectionKey: 'discordNode',
-//     accessTokenKey: 'webhookURL',
-//   },
-//   {
-//     title: 'Notion',
-//     description: 'Create entries in your notion dashboard and automate tasks.',
-//     image: '/notion.png',
-//     connectionKey: 'notionNode',
-//     accessTokenKey: 'accessToken',
-//   },
-//   {
-//     title: 'Slack',
-//     description:
-//       'Use slack to send notifications to team members through your own custom bot.',
-//     image: '/slack.png',
-//     connectionKey: 'slackNode',
-//     accessTokenKey: 'slackAccessToken',
-//     slackSpecial: true,
-//   },
-// ]
+ export const CONNECTIONS: Connection[] = [
+   {
+     title: 'Google Drive',
+     description: 'Connect your google drive to listen to folder changes',
+     image: '/googleDrive.png',
+     connectionKey: 'googleNode',
+     alwaysTrue: true,
+   },
+   {
+     title: 'Discord',
+     description: 'Connect your discord to send notification and messages',
+     image: '/discord.png',
+     connectionKey: 'discordNode',
+     accessTokenKey: 'webhookURL',
+   },
+   {
+     title: 'Notion',
+     description: 'Create entries in your notion dashboard and automate tasks.',
+     image: '/notion.png',
+     connectionKey: 'notionNode',
+     accessTokenKey: 'accessToken',
+   },
+   {
+     title: 'Slack',
+     description:
+       'Use slack to send notifications to team members through your own custom bot.',
+     image: '/slack.png',
+     connectionKey: 'slackNode',
+     accessTokenKey: 'slackAccessToken',
+     slackSpecial: true,
+   },
+ ]
