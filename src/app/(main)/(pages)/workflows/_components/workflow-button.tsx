dfbcf59ffb6b5e1212f681a99dd8c371d1ccfc57
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-13 15:36:11 +0300

    custom-modal.tsx oluşturuldu

--- src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow-button.tsx
@@ -1,12 +1,20 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { useModal } from "@/providers/modal-provider";
 import { Plus } from "lucide-react";
 import React from "react";
 
 type Props = {};
 
 const WorkflowButton = (props: Props) => {
+  const { setOpen, setClose } = useModal();
+
+  const handleClick = () => {
+    setOpen(
+      <CustomModal
+    )}
+
   return (
     <Button size={"icon"}>
       <Plus />
