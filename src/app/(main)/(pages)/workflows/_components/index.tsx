196e59b3e04b96633cf9f9a90342b33bb701cdf9
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-13 16:04:18 +0300

    workflow.tsx oluşturuldu

---
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Workflows = (props: Props) => {
+  return (
+    <div className="relative flex flex-col gap-4">
+      <section className="flex flex-col gap-4 p-6"></section>
+    </div>
+  );
+};
+
+export default Workflows;
