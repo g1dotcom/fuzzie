39ba840db01341db7d1346db183d683731c75e6c
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-13 19:04:22 +0300

    editor için gerekli componentler oluşturuldu

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/editor-canvas.tsx
@@ -0,0 +1,35 @@
+import { EditorNodeType } from "@/lib/types";
+import { useEditor } from "@/providers/editor-provider";
+import React, { useMemo } from "react";
+
+type Props = {};
+
+const initialNodes: EditorNodeType[] = [];
+
+const initialEdges: { id: string; source: string; target: string }[] = [];
+
+const EditorCanvas = (props: Props) => {
+  const { dispatch, state } = useEditor();
+
+  const nodeTypes = useMemo(
+    () => ({
+      Action: EditorCanvasCardSingle,
+      Trigger: EditorCanvasCardSingle,
+      Email: EditorCanvasCardSingle,
+      Condition: EditorCanvasCardSingle,
+      AI: EditorCanvasCardSingle,
+      Slack: EditorCanvasCardSingle,
+      "Google Drive": EditorCanvasCardSingle,
+      Notion: EditorCanvasCardSingle,
+      Discord: EditorCanvasCardSingle,
+      "Custom Webhook": EditorCanvasCardSingle,
+      "Google Calendar": EditorCanvasCardSingle,
+      Wait: EditorCanvasCardSingle,
+    }),
+    []
+  );
+
+  return <div>EditorCanvas</div>;
+};
+
+export default EditorCanvas;
