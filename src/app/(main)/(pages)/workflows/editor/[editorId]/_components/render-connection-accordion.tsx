43c4484960a30a80d87d21bd49c879c5115c2f54
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-14 16:06:37 +0300

    shadcn paketleri yuklendi

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/render-connection-accordion.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/render-connection-accordion.tsx
@@ -1,10 +1,35 @@
 "use client";
 import React from "react";
-
+import ConnectionCard from "@/app/(main)/(pages)/connections/_components/connection-card";
+import { AccordionContent } from "@/components/ui/accordion";
+import MultipleSelector from "@/components/ui/multiple-selector";
 import { Connection } from "@/lib/types";
-
-import { EditorState } from "@/providers/editor-provider";
 import { useNodeConnections } from "@/providers/connections-provider";
+import { EditorState } from "@/providers/editor-provider";
+import { useFuzzieStore } from "@/store";
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+];
 
 const RenderConnectionAccordion = ({
   connection,
@@ -24,8 +49,59 @@ const RenderConnectionAccordion = ({
   } = connection;
 
   const { nodeConnection } = useNodeConnections();
+  const { slackChannels, selectedSlackChannels, setSelectedSlackChannels } =
+    useFuzzieStore();
 
-  return <div className=""></div>;
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
+
+  const connectionData = (nodeConnection as any)[connectionKey];
+
+  const isConnected =
+    alwaysTrue ||
+    (nodeConnection[connectionKey] &&
+      accessTokenKey &&
+      connectionData[accessTokenKey!]);
+
+  return (
+    <AccordionContent key={title}>
+      {state.editor.selectedNode.data.title === title && (
+        <>
+          <ConnectionCard
+            title={title}
+            icon={image}
+            description={description}
+            type={title}
+            connected={{ [title]: isConnected }}
+          />
+          {slackSpecial && isConnected && (
+            <div className="p-6">
+              {slackChannels?.length ? (
+                <>
+                  <div className="mb-4 ml-1">
+                    Select the slack channels to send notification and messages:
+                  </div>
+                  <MultipleSelector
+                    value={selectedSlackChannels}
+                    onChange={setSelectedSlackChannels}
+                    defaultOptions={slackChannels}
+                    placeholder="Select channels"
+                    emptyIndicator={
+                      <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                        no results found.
+                      </p>
+                    }
+                  />
+                </>
+              ) : (
+                "No Slack channels found. Please add your Slack bot to your Slack channel"
+              )}
+            </div>
+          )}
+        </>
+      )}
+    </AccordionContent>
+  );
 };
 
 export default RenderConnectionAccordion;
