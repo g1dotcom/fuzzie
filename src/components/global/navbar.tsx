086472d9a9e5370dcd14ac23b66c2c1fbf1f1cbb
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-15 15:20:40 +0300

    navbar içine user tanımlandı ve userbutton eklendi

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { MenuIcon } from "lucide-react";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
 type Props = {};
 
 const Navbar = async (props: Props) => {
+  const user = await currentUser();
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
@@ -48,9 +50,10 @@ const Navbar = async (props: Props) => {
         >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-            {true ? "Dashboard" : "Get Started"}
+            {user ? "Dashboard" : "Get Started"}
           </span>
         </Link>
+        {user ? <UserButton afterSignOutUrl="/" /> : null}
         <MenuIcon className="md:hidden" />
       </aside>
     </header>
