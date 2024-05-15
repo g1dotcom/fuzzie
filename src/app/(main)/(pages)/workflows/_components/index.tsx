517ac3eb786105fed755e3badffb47617153c57d
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-15 21:57:51 +0300

    .

--- src/app/(main)/(pages)/workflows/_components/index.tsx
+++ src/app/(main)/(pages)/workflows/_components/index.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Workflow from "./workflow";
+import { onGetWorkflows } from "../_actions/workflow-connections";
 
 type Props = {};
 
-const Workflows = (props: Props) => {
+const Workflows = async (props: Props) => {
+  const workflows = await onGetWorkflows();
   return (
     <div className="relative flex flex-col gap-4">
       <section className="flex flex-col m-2">
-        <Workflow
-          description="Creating a test worklow"
-          id="kdlfnsdlknflkdsn"
-          name="Automation Workflow"
-          publish={false}
-        />
+        {workflows?.length ? (
+          workflows.map((flow) => <Workflow key={flow.id} {...flow} />)
+        ) : (
+          <div className="mt-28 flex text-muted-foreground items-center justify-center">
+            No Workflows
+          </div>
+        )}
       </section>
     </div>
   );
