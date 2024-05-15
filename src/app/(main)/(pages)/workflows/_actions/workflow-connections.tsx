4410fa8cdb9975bd66b1c59003deb8d76e83cde0
Gökhan Süle <thegkhnsl@gmail.com>
2024-05-15 15:06:10 +0300

    onFlowPublish fonksiyonu tanımlandı ve kullanıldı

--- src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
+++ src/app/(main)/(pages)/workflows/_actions/workflow-connections.tsx
@@ -19,3 +19,17 @@ export const getGoogleListener = async () => {
     if (listener) return listener;
   }
 };
+export const onFlowPublish = async (workflowId: string, state: boolean) => {
+  console.log(state);
+  const published = await db.workflows.update({
+    where: {
+      id: workflowId,
+    },
+    data: {
+      publish: state,
+    },
+  });
+
+  if (published.publish) return "Workflow published";
+  return "Workflow unpublished";
+};
--- src/app/(main)/(pages)/workflows/_components/workflow.tsx
+++ src/app/(main)/(pages)/workflows/_components/workflow.tsx
@@ -9,6 +9,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
+import { onFlowPublish } from "../editor/[editorId]/_actions/workflow-connections";
+import { toast } from "sonner";
 // import { Switch } from '@/components/ui/switch'
 // import { toast } from 'sonner'
 // import { onFlowPublish } from '../_actions/workflow-connections'
@@ -21,13 +23,13 @@ type Props = {
 };
 
 const Workflow = ({ description, id, name, publish }: Props) => {
-  //   const onPublishFlow = async (event: any) => {
-  //     const response = await onFlowPublish(
-  //       id,
-  //       event.target.ariaChecked === 'false'
-  //     )
-  //     if (response) toast.message(response)
-  //   }
+  const onPublishFlow = async (event: any) => {
+    const response = await onFlowPublish(
+      id,
+      event.target.ariaChecked === "false"
+    );
+    if (response) toast.message(response);
+  };
 
   return (
     <Card className="flex w-full items-center justify-between">
